9f495f506681659e0a0fbefd955f2a91e92e7945
rangermauve <RangerMauve@hotmail.com>
2019-03-13 15:32:08 -0400

    Uniform initialization

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # random-access-web
 Chooses the fastest random access backend based on the user's browser
+
+```shell
+npm install --save random-access-web
+```
+
+```js
+const RandomAccessWeb = require('random-access-web')
+
+const storage = await RandomAccessWeb.init()
+
+const dat = new DatJs({
+  db: storage
+})
+```
--- index.js
+++ index.js
@@ -3,10 +3,26 @@ const global = (typeof window !== 'undefined') ? window : self;
 const requestFileSystem = global.requestFileSystem || global.webkitRequestFileSystem
 const mutableFile = global.IDBMutableFile
 
+const DEFAULT_DB_NAME = 'random-access-web'
+
+let init = async (options={}) => {
+  if(typeof options === 'string') options = {name: options}
+  const name = options.name || DEFAULT_DB_NAME
+  return require('random-access-idb')(name)
+}
+
 if(requestFileSystem) {
-  module.exports = require('random-access-chrome-file')
-}  else if(mutableFile) {
-  module.exports = require('@sammacbeth/random-access-idb-mutable-file')
-} else {
-  module.exports = require('random-access-idb')
+  init = async (options) => {
+    return require('random-access-chrome-file')
+  }
+} else if(mutableFile) {
+  init = async (options) => {
+    const RandomAccess = require('@sammacbeth/random-access-idb-mutable-file')
+
+    return RandomAccess.mount()
+  }
+}
+
+module.exports = {
+  init
 }
