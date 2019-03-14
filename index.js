e3a24dda550b910a36b25cbb1cd190ea818bac65
Allain Lalonde <allain.lalonde@gmail.com>
2019-03-14 15:24:23 -0400

    refactoring into backend candidates

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
-const global = (typeof window !== 'undefined') ? window : self;
-
-const requestFileSystem = global.requestFileSystem || global.webkitRequestFileSystem
-const mutableFile = global.IDBMutableFile
+const global = typeof window !== 'undefined' ? window : self
 
 const DEFAULT_DB_NAME = 'random-access-web'
 
-let init = async (options={}) => {
-  if(typeof options === 'string') options = {name: options}
+async function init (options) {
+  const candidates = [
+    buildChromeFileStorage,
+    buildIDBMutableFileStorage,
+    buildIDBStorage
+  ]
+  let storage = null
+  for (const candidate of candidates) {
+    const storage = await candidate(options)
+    if (storage) return storage
+  }
+
+  throw new Error('Unable to build Web Storage')
+}
+
+async function buildChromeFileStorage () {
+  const requestFileSystem =
+    global.requestFileSystem || global.webkitRequestFileSystem
+  if (!requestFileSystem) return null
+
+  // FIX: catch when user does not grant permissions
+  return require('random-access-chrome-file')
+}
+
+async function buildIDBStorage (options) {
+  if (typeof options === 'string') options = { name: options }
   const name = options.name || DEFAULT_DB_NAME
   return require('random-access-idb')(name)
 }
 
-if(requestFileSystem) {
-  init = async (options) => {
-    return require('random-access-chrome-file')
-  }
-} else if(mutableFile) {
-  init = async (options) => {
-    const RandomAccess = require('@sammacbeth/random-access-idb-mutable-file')
+async function buildIDBMutableFileStorage (options) {
+  const mutableFile = global.IDBMutableFile
+  if (!mutableFile) return null
 
-    return RandomAccess.mount()
-  }
+  const randomaccess = require('@sammacbeth/random-access-idb-mutable-file')
+
+  return randomaccess.mount()
 }
 
-module.exports = {
-  init
-}
+module.exports = init
