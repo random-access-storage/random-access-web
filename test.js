e04789e77f44f6bda8fd2e8816724622b8c5f0d8
DougA <douganderson444@gmail.com>
2020-10-02 10:09:18 -0400

    add drive.ready to test

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
-var test = require('random-access-test')
-const RAW = require('./')
-const tape = require('tape')
-const hyperdrive = require('hyperdrive')
+var test = require("random-access-test");
+const RAW = require("./");
+const tape = require("tape");
+const hyperdrive = require("hyperdrive");
 
-const storage = RAW('tests-' + Math.random())
+const storage = RAW("tests-" + Math.random());
 
 test(
   function (name, options, callback) {
-    callback(storage(name, options))
+    callback(storage(name, options));
   },
   {
     // Choose which test to exercise
@@ -16,21 +16,24 @@ test(
     del: true, // tests that excersise advisory del API
     writable: true, // tests that excersise open with `options.writable`
     size: true, // tests that excersise open with `options.size`
-    truncate: false // tests that excersise open with `options.truncate`
+    truncate: false, // tests that excersise open with `options.truncate`
   }
-)
+);
 
-tape('Works with hyperdrive', (t) => {
-  const storage = RAW('tests-hyperdrive-' + Math.random())
+tape("Works with hyperdrive", (t) => {
+  const storage = RAW("tests-hyperdrive-" + Math.random());
 
-  const archive = hyperdrive(storage)
-
-  archive.writeFile('/example.txt', 'Hello World!', (err) => {
-    t.notOk(err, 'able to write')
-    archive.readFile('/example.txt', 'utf8', (err2, result) => {
-      t.notOk(err2, 'able to read')
-      t.equals(result, 'Hello World!')
-      t.end()
-    })
-  })
-})
+  const drive = hyperdrive(storage);
+  drive.ready((err) => {
+    t.error(err, "drive ready, no error");
+    t.ok(drive.writable, 'drive is writable')
+    drive.writeFile("/example.txt", "Hello World!", (err1) => {
+      t.error(err1, "able to write");
+      drive.readFile("/example.txt", "utf8", (err2, result) => {
+        t.error(err2, "able to read");
+        t.equals(result, "Hello World!");
+        t.end();
+      });
+    });
+  });
+});
